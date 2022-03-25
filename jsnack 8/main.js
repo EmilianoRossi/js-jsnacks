7d5fdc7fa152a297e35f722366bb0baecc88cf49
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-03-25 16:56:43 +0100

    correzione codice dichiarazioni function

--- jsnack 8/main.js
+++ jsnack 8/main.js
@@ -1,11 +1,12 @@
 let arrayDieciNumeri=[0 , 1 , 2  , 3 , 4 , 5 , 6 , 7 , 8 , 9];
 
 let somma=0;
-let numeri=sommaRisultati(somma , arrayDieciNumeri);
+let numeri=sommaRisultati(somma);
 function sommaRisultati(sommaArray){
 for(let i= 0 ; i<arrayDieciNumeri.length ; i++){
     sommaArray=sommaArray + arrayDieciNumeri[i];
 }
 alert("La somma dei numeri dell'array è: " + sommaArray);
 }
+
 
