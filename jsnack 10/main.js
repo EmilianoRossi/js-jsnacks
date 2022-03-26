07e4b726c670334e2d15a6efadd4e1c510008038
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-03-26 19:25:15 +0100

    js scomposizione numeri e somma

---
+++ jsnack 10/main.js
@@ -0,0 +1,8 @@
+var numeroInserito = parseInt(prompt("imserisci un numero di 4 cifre"));
+    somma = 0;
+
+while (numeroInserito) {
+    somma += numeroInserito % 10;
+    numeroInserito = Math.floor(numeroInserito / 10);
+}
+alert(somma);
